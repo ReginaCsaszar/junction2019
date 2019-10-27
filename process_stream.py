2df4a9ceafc10845b437bf2fc5cd549b1eb4d4b6
Marcell an <afga93+cc@gmail.com>
2019-10-27 08:56:40 +0100

    added mask frame

--- process_stream.py
+++ process_stream.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 
-video_uri = '/mnt/c/data/jx/vonat22.mp4'
-#video_uri = 'http://192.168.0.190:8080/stream/video.mjpeg'
+#video_uri = '/mnt/c/data/jx/vonat22.mp4'
+video_uri = 'http://192.168.0.190:8080/stream/video.mjpeg'
+
+mask = [[0.45, 0.65],[0.50,0.65],[0.6,0.95],[0.35,0.95]]
 
 def can_we_go_now(sensor_id: int) -> bool:
     video = cv2.VideoCapture(video_uri)
@@ -19,7 +21,7 @@ def can_we_go_now(sensor_id: int) -> bool:
         frame = cv2.cvtColor(frameYCrCb, cv2.COLOR_YCrCb2BGR)
         # convert to hsv for detection
         frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame_threshold = cv2.inRange(frameHSV, np.array([150,0,0]), np.array([200,100,250]))
+        frame_threshold = cv2.inRange(frameHSV, np.array([0,80,0]), np.array([255,220,255]))
         # erode/dilate for nose reduction
         frame_threshold = cv2.erode(frame_threshold, np.array([5,5]))
         frame_threshold = cv2.dilate(frame_threshold, np.array([5,5]))
@@ -40,15 +42,22 @@ if __name__ == '__main__':
         frame = cv2.cvtColor(frameYCrCb, cv2.COLOR_YCrCb2BGR)
         cv2.imshow('histeq', frame)
         frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame_threshold = cv2.inRange(frameHSV, np.array([150,0,0]), np.array([200,100,250]))
+        cv2.imshow('hsv', frameHSV)
+        frame_threshold = cv2.inRange(frameHSV, np.array([0,80,0]), np.array([255,220,255]))
         frame_threshold = cv2.erode(frame_threshold, np.array([5,5]))
         frame_threshold = cv2.dilate(frame_threshold, np.array([5,5]))
         cv2.imshow('filtered',frame_threshold)
+        for i in range(-1,3,1):
+            cv2.line(frame,
+                (int(mask[i][0]*frame.shape[1]),int(mask[i][1]*frame.shape[0])),
+                (int(mask[i+1][0]*frame.shape[1]),int(mask[i+1][1]*frame.shape[0])),
+                (0,0,255))
+        cv2.imshow('area', frame)
         key = cv2.waitKey(30)
         if key is 27: # ESC
             break
-        if key is ord(p):
+        if key is ord('p'):
             cv2.waitKey(0)
-            
+
     if key is not 27: # if not ESC wait for keypress
         cv2.waitKey(0)
